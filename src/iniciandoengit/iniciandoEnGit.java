dc95eddde0a157b763ed05375c750812a75cb182
RaulCastro <raulcgstand@gmail.com>
2023-01-15 14:48:27 +0100

    Añadiendo lineas a iniciandoEnGit.java

--- src/iniciandoengit/iniciandoEnGit.java
+++ src/iniciandoengit/iniciandoEnGit.java
@@ -3,7 +3,8 @@ package iniciandoengit;
 public class iniciandoEnGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo soy Raul");
 
 	}
 
